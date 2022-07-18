7b9f5501822880bfb540e882cba5d863b3f0c8ef
joechaaya <99142621+joechaaya@users.noreply.github.com>
2022-07-18 13:11:23 -0400

    Add files via upload
    Updated server.js

--- server.js
+++ server.js
@@ -1,21 +1,28 @@
+const apiRoutes = require('./routes/apiRoutes/');
+const htmlRoutes = require('./routes/htmlRoutes/');
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
-const apiRoutes = require('./routes/apiRoutes/noteRoutes');
-const htmlRoutes = require('./routes/htmlRoutes/index');
-const app = express();
-
 
 const PORT = process.env.PORT || 3001;
+const app = express();
 
-
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
 app.use(express.static('public'));
-
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+  });  
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+
 app.listen(PORT, () => {
-    console.log('API server now on port 3001')
+  console.log(`API server now on port ${PORT}!`);
 });
 
