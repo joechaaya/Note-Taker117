0a42def211277667b46660797eefaef18d1d4ce6
joechaaya <99142621+joechaaya@users.noreply.github.com>
2022-07-18 14:22:00 -0400

    Add files via upload
    updated api routes

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,15 +1,15 @@
 const router = require('express').Router();
-const db = require('../apiRoutes/db.json')
-const { filterById, createNote, removeNote, editNote, validateNote } = require('../notes');
+const { notes } = require('../../db/db')
+const { v4: uuidv4 } = require('uuid');
+const { createNote, removeNote, editNote, filterById } = require('../../lib/notes.js');
 
 
 router.get('/notes', (req, res) => {
-    let results = db
-    res.json(results)
+    res.json(notes)
 });
 
 router.get('/notes/:id', (req, res) => {
-    const result = filterById(req.params.id, results);
+    const result = filterById(req.params.id, notes);
     if (result) {
         res.json(result)
     } else {
@@ -18,19 +18,20 @@ router.get('/notes/:id', (req, res) => {
 })
 
 router.post('/notes', (req, res) => {
-    var results = db
-    if (validateNote(req.body)) {
-        result= createNote(req.body, results)
-        res.json(result)
-    } else{
-        res.sendStatus(404)
+    if (!req.body.id) {
+        req.body.id = uuidv4();
+        createNote(req.body, notes);
+    } else {
+        editNote(req.body, notes);
     }
+    res.json(req.body)
 })
 
 router.delete('/notes/:id', (req, res) => {
-    var results = db
-    result = deleteNote(req.params.id, results)
-    res.send(result)
+    const result = filterById(req.params.id, notes);
+
+    removeNote(result, notes);
+    res.json();
 })
 
-module.exports = router
+module.exports = router;
